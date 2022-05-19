898f2448a72e42e87db6cb7650593f800cd0e320
Shusuke Takahashi <dev@pois.jp>
2022-05-19 23:13:01 +0900

    Initial commit

---
+++ src/main/java/jp/pois/TestClass.java
@@ -0,0 +1,7 @@
+package jp.pois;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ src/test/java/jp/pois/TestClassTest.java
@@ -0,0 +1,11 @@
+package jp.pois;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestClassTest {
+    @Test
+    public void testFoo() {
+        Assertions.fail();
+    }
+}
